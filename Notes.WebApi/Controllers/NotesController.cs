0d27f37fe82003301d747e429868ab2debc5400f
yeshnapeen <45140485+yeshnapeen@users.noreply.github.com>
2020-11-09 09:01:08 +1100

    Add files via upload

---
+++ Notes.WebApi/Controllers/NotesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Notes.Core;
+using Notes.DB;
+
+namespace Notes.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private readonly ILogger<NotesController> _logger;
+        private INotesServices _notesServices;
+
+        public NotesController(ILogger<NotesController> logger, INotesServices notesServices)
+        {
+            _logger = logger;
+            _notesServices = notesServices;
+        }
+
+        [HttpGet]
+        public IActionResult GetNotes()
+        {
+            return Ok(_notesServices.GetNotes());
+        }
+
+        [HttpGet("{id}", Name ="GetNote")]
+        public IActionResult GetNote(int id)
+        {
+            return Ok(_notesServices.GetNote(id));
+        }
+
+        [HttpPost]
+        public IActionResult CreateNote(Note note)
+        {
+            var newNote = _notesServices.CreateNote(note);
+            return CreatedAtRoute("GetNote", new { newNote.Id }, newNote);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNote(int id)
+        {
+            _notesServices.DeleteNote(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult EditNote([FromBody] Note note)
+        {
+            _notesServices.EditNote(note);
+            return Ok();
+        }
+    }
+}
